6790bb7f0ddb24d39a1ab7cdbe9d94eb79669ef3
shot9 <brunoantunesrj@gmail.com>
2025-02-03 20:45:54 -0300

    removed comments

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function openModal() {
 function closeModal() {
     document.getElementById("resumeModal").style.display = "none";
 }
-
-// Fechar ao clicar fora do modal
 window.onclick = function(event) {
     var modal = document.getElementById("resumeModal");
     if (event.target === modal) {
