ff0c8e8fa768c3f23f18ceb83c8b459d58a3fbee
shot9 <brunoantunesrj@gmail.com>
2025-02-03 15:39:21 -0300

    particles added

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ window.onclick = function(event) {
         closeModal();
     }
 };
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: "#ffffff" },
+      shape: { type: "circle" },
+      opacity: { value: 0.5, random: true },
+      size: { value: 3, random: true },
+      move: { enable: true, speed: 2 }
+    },
+    interactivity: {
+      events: {
+        onhover: { enable: true, mode: "repulse" }
+      }
+    }
+  });
